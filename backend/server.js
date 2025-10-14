465087e0659f7d313c532d872f56e287881ca3c9
Lakhan Tekchandani <lakhantekchandani400@gmail.com>
2025-10-14 17:39:43 +0530

    Final CORS and env cleanup

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [
+     'http://localhost:3000',   
     'https://sahaayakincrediblewebsite.netlify.app' // <-- add your Netlify domain (no trailing slash)
   ],
   credentials: true
