361deb052d62d7a497de35a93f7cacf28929b521
Lakhan Tekchandani <lakhantekchandani400@gmail.com>
2025-10-14 19:07:15 +0530

    Update CORS for deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-     'http://localhost:3000',   
-    'https://sahaayakincrediblewebsite.netlify.app' // <-- add your Netlify domain (no trailing slash)
-  ],
+  origin: 'https://sahaayakwomenswebsite.netlify.app',
   credentials: true
 }));
 
